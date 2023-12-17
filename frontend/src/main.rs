a1c4a0f513477a2c02d1705cded272b7f5a37426
IshariFluttershy <tanguy.morvant2@gmail.com>
2023-12-17 12:07:55 +0100

    frontend select implemented

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,4 +1,7 @@
-use yew::prelude::*;
+use web_sys::{EventTarget, HtmlSelectElement};
+use yew::{prelude::*, html::IntoPropValue};
+use wasm_bindgen::JsCast;
+use gloo_console::log;
 
 #[derive(Properties, PartialEq)]
 pub struct Props {
@@ -17,21 +20,48 @@ fn App() -> Html {
         }
     };
 
+    let goods_type_handle = use_state(String::default);
+    let payment_type_handle = use_state(String::default);
+
+    let on_goods_type_change = {
+        let goods_type_handle = goods_type_handle.clone();
+
+        Callback::from(move |e: Event| {
+            let target: Option<EventTarget> = e.target();
+            let input = target.and_then(|t| t.dyn_into::<HtmlSelectElement>().ok());
+            if let Some(input) = input {
+                goods_type_handle.set(input.value());
+            }
+        })
+    };
+
+    let on_payment_type_change = {
+        let payment_type_handle = payment_type_handle.clone();
+
+        Callback::from(move |e: Event| {
+            let target: Option<EventTarget> = e.target();
+            let input = target.and_then(|t| t.dyn_into::<HtmlSelectElement>().ok());
+            if let Some(input) = input {
+                payment_type_handle.set(input.value());
+            }
+        })
+    };
+
     html! {
         <div>
             <input type="text" id="Name" name="Name" placeholder="Prix"/>
-            <select name="model" id="model-select" onchange={props.payment_method}>
-                <option value="stable_diffusion_onnx" selected={true}>{"Stable Diffusion"}</option>
-                <option value="waifu-diffusion-diffusers-onnx-v1-3">{"Waifu Diffusion"}</option>
-                <option value="hassanblend_onnx">{"Hassanblend"}</option>
+            <select name="model" id="model-select" onchange={on_goods_type_change}>
+                <option value="Nourriture" selected={true}>{"Nourriture"}</option>
+                <option value="Charges">{"Charges"}</option>
+                <option value="Autres">{"Autres"}</option>
             </select>
-            <select name="model" id="model-select" onchange={goods_type}>
-                <option value="stable_diffusion_onnx" selected={true}>{"Stable Diffusion"}</option>
-                <option value="waifu-diffusion-diffusers-onnx-v1-3">{"Waifu Diffusion"}</option>
-                <option value="hassanblend_onnx">{"Hassanblend"}</option>
+            <select name="model" id="model-select" onchange={on_payment_type_change}>
+                <option value="Carte bleue" selected={true}>{"Carte bleue"}</option>
+                <option value="Especes">{"Especes"}</option>
             </select>
             <button {onclick}>{ "Valider" }</button>
-            <p>{ *counter }</p>
+            <p>{ (*goods_type_handle).clone() }</p>
+            <p>{ (*payment_type_handle).clone() }</p>
         </div>
     }
 }
