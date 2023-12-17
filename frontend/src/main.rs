93d70fc63849e83eb11c64a8be1ebc42cc8163d7
IshariFluttershy <tanguy.morvant2@gmail.com>
2023-12-17 12:56:28 +0100

    frontend can send request

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,6 +1,7 @@
 use web_sys::{EventTarget, HtmlSelectElement};
-use yew::{prelude::*, html::IntoPropValue};
+use yew::{prelude::*, html::IntoPropValue, platform::spawn_local};
 use wasm_bindgen::JsCast;
+use reqwasm::http::*;
 use gloo_console::log;
 
 #[derive(Properties, PartialEq)]
@@ -12,7 +13,8 @@ pub struct Props {
 #[function_component]
 fn App() -> Html {
     let counter = use_state(|| 0);
-    let onclick = {
+
+    /*let onclick = {
         let counter = counter.clone();
         move |_| {
             let value = *counter + 1;
@@ -20,8 +22,34 @@ fn App() -> Html {
         }
     };
 
+    let test = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };*/
+
     let goods_type_handle = use_state(String::default);
     let payment_type_handle = use_state(String::default);
+
+    let onclick = {
+        let payment_type_handle = payment_type_handle.clone();
+        move |_| {
+            let payment_type_handle = payment_type_handle.clone();
+            spawn_local(async move {
+                let resp = Request::post("/command")
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .body(wasm_bindgen::JsValue::from_str(
+                        &format!("payment_type={}",
+                        *payment_type_handle,
+                        )))
+                    .send()
+                    .await
+                    .unwrap();
+            });
+        }
+    };
 
     let on_goods_type_change = {
         let goods_type_handle = goods_type_handle.clone();
@@ -61,7 +89,6 @@ fn App() -> Html {
             </select>
             <button {onclick}>{ "Valider" }</button>
             <p>{ (*goods_type_handle).clone() }</p>
-            <p>{ (*payment_type_handle).clone() }</p>
         </div>
     }
 }
