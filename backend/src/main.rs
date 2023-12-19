b3f225e1b4f20b25ade563bab7c5ed26e186af52
IshariFluttershy <tanguy.morvant2@gmail.com>
2023-12-19 22:02:31 +0100

    basic backend implemented frontend now send and retreive payments

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,35 @@
 use std::path::PathBuf;
-
-use rocket::{fs::NamedFile, response::status::NotFound};
+use std::sync::Arc;
+use rocket::State;
+use rocket::form::Form;
+use rocket::tokio::sync::Mutex;
+use rocket::{fs::NamedFile, response::{status::NotFound, content::RawHtml}};
 
 #[macro_use] extern crate rocket;
+
+#[derive(Debug)]
+struct PaymentEntry {
+    price: f64,
+    goods_type: String,
+    payment_type: String,
+}
+
+#[derive(Debug)]
+struct PaymentDatas {
+    payments: Vec<PaymentEntry>,
+}
+
+impl PaymentDatas {
+    fn new() -> PaymentDatasPointer {
+        let new_payment_datas = PaymentDatas {
+            payments: Vec::new(),
+        };
+        Arc::new(Mutex::new(new_payment_datas))
+    }
+}
+
+type PaymentDatasPointer = Arc<Mutex<PaymentDatas>>;
+
 
 // Return the index when the url is /
 #[get("/")]
@@ -10,11 +37,44 @@ async fn index() -> Result<NamedFile, NotFound<String>> {
     get_index().await
 }
 
+#[derive(FromForm, Debug)]
+struct PaymentEntryRequest<'r> {
+    r#price: f64,
+    r#goods_type: &'r str,
+    r#payment_type: &'r str,
+}
+
+
+
+#[post("/command", data = "<payment_entry>")]
+async fn command(payment_entry: Form<PaymentEntryRequest<'_>>, payment_datas: &State<PaymentDatasPointer>) -> RawHtml<String> {
+    println!("youpi !");
+    println!("data : {}", payment_entry.payment_type);
+    let entry = PaymentEntry {
+        price: payment_entry.price,
+        goods_type: payment_entry.goods_type.to_string(),
+        payment_type: payment_entry.payment_type.to_string(),
+    };
+    let mut payment_datas = payment_datas.lock().await;
+    payment_datas.payments.push(entry);
+
+
+    println!("payment_datas.payments : {:#?}", payment_datas.payments);
+
+    RawHtml(format!("payment_datas.payments : {:#?}", payment_datas.payments))
+}
+
+#[get("/test")]
+async fn test() -> RawHtml<String> {
+    RawHtml(format!("connected"))
+}
+
 #[launch]
 fn rocket() -> _ {
     // You must mount the static_files route
     rocket::build()
-        .mount("/", routes![index, static_files])
+        .manage(PaymentDatas::new())
+        .mount("/", routes![index, static_files, command, test])
 }
 
 // Return the index file as a Rocket NamedFile
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -12,7 +12,7 @@ pub struct Props {
 
 #[function_component]
 fn App() -> Html {
-    let counter = use_state(|| 0);
+    let price = use_state(|| 0.);
 
     /*let onclick = {
         let counter = counter.clone();
@@ -33,20 +33,38 @@ fn App() -> Html {
     let goods_type_handle = use_state(String::default);
     let payment_type_handle = use_state(String::default);
 
+    let payment_data_handle = use_state(String::default);
+
+
     let onclick = {
+        let price = price.clone();
+        let goods_type_handle = goods_type_handle.clone();
         let payment_type_handle = payment_type_handle.clone();
+        let payment_data_handle = payment_data_handle.clone();
+
+
         move |_| {
+            let price = price.clone();
+            let goods_type_handle = goods_type_handle.clone();
             let payment_type_handle = payment_type_handle.clone();
+            let payment_data_handle = payment_data_handle.clone();
+
+
             spawn_local(async move {
                 let resp = Request::post("/command")
                     .header("Content-Type", "application/x-www-form-urlencoded")
                     .body(wasm_bindgen::JsValue::from_str(
-                        &format!("payment_type={}",
+                        &format!("price={}&goods_type={}&payment_type={}",
+                        *price,
+                        *goods_type_handle,
                         *payment_type_handle,
                         )))
                     .send()
                     .await
                     .unwrap();
+
+                //log!(resp.text().await.unwrap());
+                payment_data_handle.set(resp.text().await.unwrap());
             });
         }
     };
@@ -88,7 +106,7 @@ fn App() -> Html {
                 <option value="Especes">{"Especes"}</option>
             </select>
             <button {onclick}>{ "Valider" }</button>
-            <p>{ (*goods_type_handle).clone() }</p>
+            <p>{ (*payment_data_handle).clone() }</p>
         </div>
     }
 }
